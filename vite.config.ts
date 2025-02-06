83cd1f7c399183dde34d54a18c30395bdcaac06b
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-06 16:00:40 +0100

    creation du footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,3 +8,4 @@ export default defineConfig({
   tailwindcss(),
   ],
 });
+
