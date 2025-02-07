175702a17a16c607cebe917638c185fc12b6d4d4
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-07 13:48:13 +0100

    création de la page 404

--- src/component/ui/Card.tsx
+++ src/component/ui/Card.tsx
@@ -4,11 +4,11 @@ export default function Card() {
             <div>
                 <img className="object-cover rounded-t-lg" src="/images/arbres/chene_pedoncule.webp" alt="" />
             </div>
-            <div className="flex">
+            <div className="flex items-center">
                 <p className="text-xl font-bold p-4 font-title">
                     Chène pédonculé
                 </p>
-                <img src="images/icons/shop-card.svg" alt="Logo panier" className="w-8 bg-cta" />
+                <img src="images/icons/shop-card.svg" alt="Logo panier" className="w-7 bg-cta rounded-full" />
             </div>
         </article>
     )
