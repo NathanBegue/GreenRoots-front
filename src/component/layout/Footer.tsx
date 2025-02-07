0b72148912ec7c457d9d50571bed68370dda628b
Léo Khatchatourian <contact.leokha@gmail.com>
2025-02-07 15:00:07 +0100

    integration burger-menu + modif footer +bouton add-panier

--- src/component/layout/Footer.tsx
+++ src/component/layout/Footer.tsx
@@ -1,15 +1,10 @@
 export default function Footer() {
     return (
-        <footer className="bg-dark-third  w-full flex items-center gap-32 h-6">
-            <div className="flex  space-x-1 items-center w-4 gap-1">
-                <img className="max-h-4" src="images/icones/facebook.svg" alt="Logo de Facebook" />
-                <img className="max-h-4" src="images/icones/linkedin.svg" alt="Logo de Linkedin" />
-                <img className="max-h-4" src="images/icones/instagram.svg" alt="Logo d'Instagram" />
-            </div>
+        <footer className="bg-dark-third  w-full flex justify-center items-center  h-12 shadow-black shadow-lg">
+            <p className="font-content flex"> GreenRoots
+                <img src="images/icons/copyright.svg" alt="" className="w-5 h-5" />
+            </p>
 
-            <p className="font-content text-xs flex"> GreenRoots <img src="images/icones/copyright.svg" alt="" className="w-2" /></p>
-
-            <a href="" className="font-content size-4"> CGU</a>
         </footer>
 
     )
