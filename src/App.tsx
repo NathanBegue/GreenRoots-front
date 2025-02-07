68619acc3a7e2273151f3e6f4f8cd919af007da1
Léo Khatchatourian <contact.leokha@gmail.com>
2025-02-07 16:46:22 +0100

    mise en place de react-router plus tentative de mettre bien le lien retour vers l'accueil de la 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from "react-router";
+import Index from "./component/pages/Index";
 import Page404 from "./component/pages/Page404";
+import Boutique from "./component/pages/Boutique";
 
 
 function App() {
   return (
-    <Page404 />
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/boutique" element={<Boutique />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 }
 
