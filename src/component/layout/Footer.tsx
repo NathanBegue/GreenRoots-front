fbc3b7851b3d41766384ffc2b6d5307ded5bc9f2
Léo Khatchatourian <contact.leokha@gmail.com>
2025-02-08 16:32:10 +0100

    page connexion integrée

--- src/component/layout/Footer.tsx
+++ src/component/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-dark-third  w-full flex justify-center items-center  h-9 shadow-black shadow-lg">
+        <footer className="bg-dark-third  w-full flex justify-center items-center  h-9 ">
             <p className="font-content flex"> GreenRoots
                 <img src="images/icons/copyright.svg" alt="" className="w-5 h-5" />
             </p>
