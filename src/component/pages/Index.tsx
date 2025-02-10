daf2718683268a7f2198c997d63ef7229e7b7e66
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-10 16:49:03 +0100

    responsive index : ok

--- src/component/pages/Index.tsx
+++ src/component/pages/Index.tsx
@@ -8,29 +8,29 @@ export default function Index() {
         <div className="w-full max-w-screen overflow-hidden">
             {/* <ConnexionModal /> */}
             <main className="bg-dark-primary text-white  flex flex-col gap-6 text-center ">
-                <section className="flex flex-col gap-6">
-                    <h1 className="text-2xl font-bold font-title pt-20">
+                <section className="flex flex-col gap-6 ">
+                    <h1 className="text-2xl font-bold font-title pt-20 lg:text-3xl">
                         🌳 GreenRoots, parce qu'un arbre planté aujourd'hui est une forêt pour demain. 🌳
                     </h1>
-                    <div className="w-screen h-screen bg-cover bg-center pt-30" style={{ backgroundImage: `url(${bgAccueil})` }}>
+                    <div className="w-screen h-screen bg-cover bg-center m-auto pt-30 p-6 md:pl-48 md:pr-48  lg:pl-50 xl:pr-50" style={{ backgroundImage: `url(${bgAccueil})` }}>
                         <div className="flex flex-col gap-6 inset-0 bg-black/60  items-center justify-center ">
-                            <h2 className="text-xl font-bold font-title">
+                            <h2 className="text-xl font-bold font-title lg:text-3xl">
                                 La déforestation, un enjeu majeur pour la planète
                             </h2>
-                            <p className="font-content pt-6">
+                            <p className="font-content pt-6 lg:text-2xl">
                                 Chaque année, 15 milliards d'arbres sont abattus, mais seulement 5 milliards sont replantés. La déforestation entraîne une perte de 10 milliards d’arbres par an, menaçant la biodiversité et accélérant la désertification. En Amazonie, 17% de la forêt a déjà disparu, et en Afrique, le taux de déforestation a augmenté de 150% en 20 ans.
                             </p>
-                            <p className="font-content">
+                            <p className="font-content lg:text-2xl">
                                 Notre Solution : Nous avons créé une application mobile qui permet de planter des arbres en un clic. Pour chaque euro dépensé, un arbre est planté. Notre objectif est de planter 1 milliard d’arbres par an, soit 1 arbre pour chaque personne connectée à Internet.
                             </p>
                         </div>
                     </div>
                 </section>
-                <section className="flex flex-col gap-6">
+                <section className="flex flex-col gap-6 p-6 md:m-auto">
                     <h2 className="text-2xl font-bold font-title">
                         Nos meilleurs arbres
                     </h2>
-                    <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-6 xl:flex-row">
                         <Card />
                         <Card />
                         <Card />
