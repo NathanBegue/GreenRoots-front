4ea9afb8cfea6c126ccec99ffe6b4c5399359df5
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-10 10:13:45 +0100

    image Landing page ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Boutique from "./component/pages/Boutique";
+
 import Index from "./component/pages/Index";
-import Page404 from "./component/pages/Page404";
+
 
 
 function App() {
   return (
-    <Page404 />
+    <Index />
   );
 }
 
