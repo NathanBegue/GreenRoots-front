4044207499797761f3ca3cb75cbd6e7ff19d208a
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-02-13 16:19:39 +0100

    responsive + filtre par categorie : ok

--- src/component/ui/Card.tsx
+++ src/component/ui/Card.tsx
@@ -1,12 +1,12 @@
 import { Itrees } from "../../../type/type.ts";
 
 export default function Card({
-    article,
-    isAdmin = true,
-    isSmall = false,
-    setIsOpenedEditModal,
-    setIsOpenedDeleteModal,
-    setSelectedArticle
+  article,
+  isAdmin = true,
+  isSmall = false,
+  setIsOpenedEditModal,
+  setIsOpenedDeleteModal,
+  setSelectedArticle
 }: {
     isAdmin?: boolean;
     isSmall?: boolean; // Gère le format compact ET la suppression des boutons
@@ -15,56 +15,59 @@ export default function Card({
     article: Itrees;
     setSelectedArticle?: React.Dispatch<React.SetStateAction<Itrees | null>>;
 }) {
-    return (
-        <article
-            className={`bg-dark-secondary text-white flex ${isSmall ? "flex-row items-center p-2 gap-2 w-full max-w-sm" : "flex-col"} 
+  return (
+    <article
+      className={`bg-dark-secondary text-white flex ${isSmall ? "flex-row items-center p-2 gap-2 w-full max-w-sm" : "flex-col"} 
             rounded-lg border shadow-black shadow-lg md:max-w-4xl`}
-        >
-            {/* Image du produit */}
-            <div>
-                <img
-                    className={`object-cover rounded-lg aspect-square ${isSmall ? "w-12 h-12" : ""}`}
-                    src={article.Picture ? `/images/arbres/${article.Picture.url}.webp` : "/images/default.jpg"}
-                    alt={article.name}
-                />
+    >
+      {/* Image du produit */}
+      <div>
+        <img
+          className={`object-cover rounded-lg aspect-square ${isSmall ? "w-12 h-12" : ""}`}
+          src={article.Picture ? `/images/arbres/${article.Picture.url}.webp` : "/images/default.jpg"}
+          alt={article.name}
+        />
 
-            </div>
+      </div>
 
-            {/* Contenu de la carte */}
-            <div className={`flex ${isSmall ? "items-center gap-2" : "gap-4"} justify-between items-center w-full p-4`}>
-                <p className={`font-title text-xs min-[374px]:text-base ${isSmall ? "text-sm" : "text-xl font-bold"}`}>{article.name}</p>
-                <p className="font-semibold text-xs min-[374px]:text-base text-cta">{`Prix: ` + article.price}</p>
+      {/* Contenu de la carte */}
+          <p className={`font-title text-3xl min-[374px]:text-base pt-4 ${isSmall ? "text-sm" : "text-xl font-bold"}`}>{article.name}</p>
+      <div className={`flex justify-between items-center w-full pb-4`}>
+               <p className="font-semibold text-xs min-[374px]:text-base text-cta ml-2">{`Prix: ` + article.price}</p>
 
-                {/* Supprime tous les boutons si isSmall est activé */}
-                {!isSmall &&
+                 {/* Supprime tous les boutons si isSmall est activé */}
+          {!isSmall &&
                     (isAdmin ? (
-                        <div className="flex gap-2">
-                            <button onClick={() => {
-                                setIsOpenedEditModal && setIsOpenedEditModal(true);
-                                setSelectedArticle && setSelectedArticle(article)
-                            }
+                      <div className="flex gap-2 mt-2">
+                        <button onClick={() => {
+                          setIsOpenedEditModal && setIsOpenedEditModal(true);
+                          setSelectedArticle && setSelectedArticle(article)
+                        }
 
-                            }
-                                className="p-2 bg-yellow-500 rounded-lg hover:bg-yellow-600 transition">
-                                <img src="/images/icons/edit.svg" alt="Modifier" className="w-6 h-6 invert" />
-                            </button>
-
-                            <button onClick={() => {
-                                setIsOpenedDeleteModal && setIsOpenedDeleteModal(true)
-                                setSelectedArticle && setSelectedArticle(article)
-                            }}
-                                className="p-2 bg-red-500 rounded-lg hover:bg-red-600 transition">
-                                <img src="/images/icons/trash.svg" alt="Supprimer" className="w-6 h-6 invert" />
-                            </button>
-                        </div>
-                    ) : (
-                        // Bouton d’ajout au panier pour l’utilisateur normal
-                        <button className="flex items-center bg-cta p-2 rounded-lg hover:bg-cta-dark transition">
-                            <img src="/images/icons/shop-card.svg" alt="Ajouter au panier" className="w-6 h-6 invert" />
+                        }
+                        className="p-2 bg-yellow-500 rounded-lg hover:bg-yellow-600 transition md:w-8 lg:w-10 lg:h-12">
+                          <img src="/images/icons/edit.svg" alt="Modifier" className="w-6 h-6 invert" />
                         </button>
+
+                        <button onClick={() => {
+                          setIsOpenedDeleteModal && setIsOpenedDeleteModal(true)
+                          setSelectedArticle && setSelectedArticle(article)
+                        }}
+                        className="p-2 bg-red-500 rounded-lg hover:bg-red-600 transition lg:w-10 lg:h-12 md:w-8 mr-2">
+                          <img src="/images/icons/trash.svg" alt="Supprimer" className="w-6 h-6 invert "/>
+                        </button>
+                      </div>
+                    ) : (
+                    // Bouton d’ajout au panier pour l’utilisateur normal
+                      <button className="flex items-center bg-cta p-2 rounded-lg hover:bg-cta-dark transition">
+                        <img src="/images/icons/shop-card.svg" alt="Ajouter au panier" className="w-6 h-6 invert" />
+                      </button>
                     ))
-                }
-            </div>
-        </article >
-    );
+          }
+
+        </div>
+
+
+    </article >
+  );
 }
--- src/fetch/method-fetch.ts
+++ src/fetch/method-fetch.ts
@@ -1,79 +1,79 @@
 import { Itrees, } from "../../type/type";
 
 const fetchmethod = {
-    getArticles: async (): Promise<Itrees[]> => {
-        try {
-            const token = localStorage.getItem("token"); // Récupération du token
+  getArticles: async (): Promise<Itrees[]> => {
+    try {
+      const token = localStorage.getItem("token"); // Récupération du token
 
-            const response = await fetch("http://localhost:5000/api/articles", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`, // 🔥 Ajout du token JWT
-                },
-            });
+      const response = await fetch("http://localhost:5000/api/articles", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`, // 🔥 Ajout du token JWT
+        },
+      });
 
-            if (!response.ok) {
-                throw new Error(`Erreur API : ${response.status} - ${await response.text()}`);
-            }
+      if (!response.ok) {
+        throw new Error(`Erreur API : ${response.status} - ${await response.text()}`);
+      }
 
-            const data = await response.json();
-            console.log("Données reçues :", data);
+      const data = await response.json();
+      console.log("Données reçues :", data);
 
-            if (data.articles) {
-                const mergedArticles = data.articles.map((article: Itrees) => ({
-                    ...article,
-                    Picture: article.Picture || { url: "/images/default.jpg", description: "Image par défaut" }
-                }));
+      if (data.articles) {
+        const mergedArticles = data.articles.map((article: Itrees) => ({
+          ...article,
+          Picture: article.Picture || { url: "/images/default.jpg", description: "Image par défaut" }
+        }));
 
-                return mergedArticles;
-            } else {
-                console.error("Format inattendu de l'API", data);
-                return [];
-            }
+        return mergedArticles;
+      } else {
+        console.error("Format inattendu de l'API", data);
+        return [];
+      }
 
-        } catch (error) {
-            console.error("Erreur lors du fetch des articles:", error);
-            return [];
-        }
-    },
-
-
-    getNewArticle: async (): Promise<Itrees[]> => {
-        try {
-            const response = await fetch("http://localhost:5000/");
-            const data = await response.json();
-
-            console.log("Données reçues :", data);
-
-            if (data.articles) {
-                const mergedArticles = data.articles.map((article: Itrees) => ({
-                    ...article,
-                    Picture: article.Picture || { url: "/images/default.jpg", description: "Image par défaut" }
-                }));
-
-                return mergedArticles;
-            } else {
-                console.error("Format inattendu de l'API", data);
-                return [];
-            }
-
-        } catch (error) {
-            console.error("Erreur lors du fetch des articles:", error);
-            return [];
-        }
-    },
-
-    getArticleByOrder: async () => {
-        try {
-            const response = await fetch("http://localhost:5000/commande");
-            const data = await response.json();
-            console.log("Données reçues:", data);
-
-        } catch (error) {
-            console.error("Erreur lors de la récupération de l'historique des commandes:", error);
-        }
+    } catch (error) {
+      console.error("Erreur lors du fetch des articles:", error);
+      return [];
     }
+  },
+
+
+  getNewArticle: async (): Promise<Itrees[]> => {
+    try {
+      const response = await fetch("http://localhost:5000/");
+      const data = await response.json();
+
+      console.log("Données reçues :", data);
+
+      if (data.articles) {
+        const mergedArticles = data.articles.map((article: Itrees) => ({
+          ...article,
+          Picture: article.Picture || { url: "/images/default.jpg", description: "Image par défaut" }
+        }));
+
+        return mergedArticles;
+      } else {
+        console.error("Format inattendu de l'API", data);
+        return [];
+      }
+
+    } catch (error) {
+      console.error("Erreur lors du fetch des articles:", error);
+      return [];
+    }
+  },
+
+  getArticleByOrder: async () => {
+    try {
+      const response = await fetch("http://localhost:5000/commande");
+      const data = await response.json();
+      console.log("Données reçues:", data);
+
+    } catch (error) {
+      console.error("Erreur lors de la récupération de l'historique des commandes:", error);
+    }
+  }
 
 };
 
